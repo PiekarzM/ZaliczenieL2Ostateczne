fa74d2fe5e2ab14c39a27f72d393bce6660a0f78
PiekarzM <127319430+PiekarzM@users.noreply.github.com>
2023-06-14 12:47:37 +0000

    z2
    z2

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x =0;
+System.out.printf("podaj liczbe");
+        int y = scan.nextInt();
+        do {
+            if(y%5==0)
+                x+=y;
+            System.out.printf("Podaj liczbe");
+            y = scan.nextInt();
+        }
+          while(y!=-5);
+        System.out.printf("Suma liczb podzielnych przez 5 "+x);
+    }
 }
+
+
+
